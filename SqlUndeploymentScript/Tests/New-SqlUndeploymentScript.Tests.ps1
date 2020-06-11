6b7e4e4c72d99d8d38b58293c57b11a7bf7e4a28
François Chabot <fchabot@skynet.be>
2020-06-11 18:09:05 +0200

    Sql Deployment and Undeployment Scripts

---
+++ SqlUndeploymentScript/Tests/New-SqlUndeploymentScript.Tests.ps1
@@ -0,0 +1,90 @@
+#region Copyright & License
+
+# Copyright © 2012 - 2020 François Chabot
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#endregion
+
+Import-Module -Name $PSScriptRoot\..\SqlUndeploymentScript -Force
+
+Describe 'New-SqlUndeploymentScript' {
+    InModuleScope SqlUndeploymentScript {
+
+        Context 'When SqlUndeploymentScript file does not exist' {
+            BeforeAll {
+                $script:ParameterBindingValidationExceptionType = [Type]::GetType('System.Management.Automation.ParameterBindingValidationException, System.Management.Automation', $true)
+            }
+            It 'Throws a ParameterBindingValidationException.' {
+                { New-SqlUndeploymentScript -Path 'z:\SqlUndeploymentScript.dll' } | Should -Throw -ExceptionType $ParameterBindingValidationExceptionType
+            }
+        }
+
+        Context 'When SqlUndeploymentScript file exists' {
+            BeforeAll {
+                # ensure Manifest variable is available while using the command outside of the New-Manifest -Build { BuildScriptBlock}
+                New-Variable -Name Manifest -Value @{ } -Scope Global
+                # create some empty files
+                '' > TestDrive:\one.sql
+                '' > TestDrive:\two.sql
+            }
+            It 'Returns a custom object with both a path and a name property.' {
+                $expectedItem = [PSCustomObject]@{ Name = 'one.sql' ; Path = 'TestDrive:\one.sql' | Resolve-Path | Select-Object -ExpandProperty ProviderPath }
+
+                $actualItem = New-SqlUndeploymentScript -Path TestDrive:\one.sql -PassThru
+
+                Compare-Item -ReferenceItem $expectedItem -DifferenceItem $actualItem | Should -BeNullOrEmpty
+            }
+            It 'Returns a collection of custom objects with both a path and a name property.' {
+                $expectedItems = @(
+                    [PSCustomObject]@{ Name = 'one.sql' ; Path = 'TestDrive:\one.sql' | Resolve-Path | Select-Object -ExpandProperty ProviderPath }
+                    [PSCustomObject]@{ Name = 'two.sql' ; Path = 'TestDrive:\two.sql' | Resolve-Path | Select-Object -ExpandProperty ProviderPath }
+                )
+
+                $actualItems = New-SqlUndeploymentScript -Path (Get-ChildItem -Path TestDrive:\) -PassThru
+
+                $actualItems | Should -HaveCount 2
+                0..1 | ForEach-Object -Process { Compare-Item -ReferenceItem $expectedItems[$_] -DifferenceItem $actualItems[$_] | Should -BeNullOrEmpty }
+            }
+            AfterAll {
+                Remove-Variable -Name Manifest -Scope Global -Force
+            }
+        }
+
+        Context 'Creating SqlUndeploymentScripts must be done via the ScriptBlock passed to New-Manifest' {
+            BeforeAll {
+                # create some empty files
+                '' > TestDrive:\one.sql
+                '' > TestDrive:\two.sql
+                '' > TestDrive:\six.sql
+            }
+            It 'Accumulates the Items as a SqlUndeploymentScripts resource collection of the Manifest being built.' {
+                $expectedItems = @(
+                    [PSCustomObject]@{ Name = 'one.sql' ; Path = 'TestDrive:\one.sql' | Resolve-Path | Select-Object -ExpandProperty ProviderPath }
+                    [PSCustomObject]@{ Name = 'six.sql' ; Path = 'TestDrive:\six.sql' | Resolve-Path | Select-Object -ExpandProperty ProviderPath }
+                    [PSCustomObject]@{ Name = 'two.sql' ; Path = 'TestDrive:\two.sql' | Resolve-Path | Select-Object -ExpandProperty ProviderPath }
+                )
+
+                $manifest = New-Manifest -Type Application -Name 'BizTalk.Factory' -Build {
+                    New-SqlUndeploymentScript -Path (Get-ChildItem -Path TestDrive:\)
+                }
+
+                $manifest | Should -Not -BeNullOrEmpty
+                $manifest.ContainsKey('SqlUndeploymentScripts') | Should -BeTrue
+                $manifest.SqlUndeploymentScripts | Should -HaveCount 3
+                0..2 | ForEach-Object -Process { Compare-Item -ReferenceItem $expectedItems[$_] -DifferenceItem $manifest.SqlUndeploymentScripts[$_] | Should -BeNullOrEmpty }
+            }
+        }
+
+    }
+}
